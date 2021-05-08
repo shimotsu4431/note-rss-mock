fb12af425c5318454f0931a4db7eafe08c18b152
shimotsu4431 <shimotsu13@gmail.com>
2021-05-08 11:39:31 +0900

    update index

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,72 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">note</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div class="wrapper">
+    <h1>All</h1>
+    <ul>
+      <li v-for="(article, index) in articlesAll" :key="index">
+        <a :href="article.link">{{ article.title[0] }}</a>
+      </li>
+    </ul>
+    <h1>Magazine</h1>
+    <ul>
+      <li v-for="(article, index) in articlesMagazine" :key="index">
+        <a :href="article.link">{{ article.title[0] }}</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {}
+import xml2js from 'xml2js'
+
+export const processors = xml2js.processors
+export const xmlParser = xml2js.Parser({
+  tagNameProcessors: [processors.stripPrefix],
+})
+
+export const BASE_KEY =
+  'https://us-central1-mediation-server.cloudfunctions.net/api'
+export const KEY_ALL = BASE_KEY + '/all'
+export const KEY_MAGAZINE = BASE_KEY + '/magazine'
+
+export default {
+  data() {
+    return {
+      articlesMagazine: [],
+      articlesAll: [],
+    }
+  },
+  async mounted() {
+    ;[this.articlesMagazine, this.articlesAll] = await Promise.all([
+      await this.fetchArticles(KEY_ALL),
+      await this.fetchArticles(KEY_MAGAZINE),
+    ])
+  },
+  methods: {
+    async fetchArticles(KEY) {
+      let result = []
+      const articles = await this.$axios.get(KEY).then((response) => {
+        xmlParser.parseString(response.data, (messages, xmlres) => {
+          result = xmlres.rss.channel[0].item
+        })
+        return result
+      })
+      return articles
+    },
+  },
+}
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+.wrapper {
+  padding: 20px;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+h1 {
+  margin-bottom: 10px;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+ul {
+  margin-bottom: 25px;
+  padding-left: 20px;
 }
 </style>
